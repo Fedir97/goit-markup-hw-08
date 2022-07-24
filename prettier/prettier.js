d7ad7619eccbee13868e76ee4cd69340e24c603c
Fedir97 <106695191+Fedir97@users.noreply.github.com>
2022-07-24 13:15:47 +0300

    Add files via upload

---
+++ prettier/prettier.js
@@ -0,0 +1,125 @@
+< !DOCTYPE html >
+    <html lang="ru">
+
+        <head>
+            <link rel="stylesheet" href="./css/style1.css" />
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
+                <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,700&display=swap" rel="stylesheet">
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+                        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;900&display=swap" rel="stylesheet">
+                            <link href="https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap" rel="stylesheet">
+                                <meta charset="UTF-8">
+                                    <link rel="stylesheet" href="./css/style1.css" />
+                                    <meta charset="UTF-8">
+                                        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                                <title>Document</title>
+                                            </head>
+
+                                            <body>
+
+                                                <header>
+                                                    <nav class="nav-bar">
+                                                        <a class="logo1" href="">Web<span class="logo-accent">Studio</span></a>
+                                                        <ul class="info">
+                                                            <li><a class="link-info" href="./index.html">Студия</a></li>
+                                                            <li><a class="link-info" href="./portfolio.html">Портфолио</a></li>
+                                                            <li><a class="link-info" href="">Контакты</a></li>
+                                                        </ul>
+                                                        <ul class="contacts">
+                                                            <li><a class='link1' href="mailto:info@devstudio.com">info@devstudio.com</a>
+                                                            </li>
+                                                            <li><a class='link1' href="tel:+380961111111">+380961111111</a>
+                                                            </li>
+                                                        </ul>
+                                                    </nav>
+                                                </header>
+                                                <section class="topic">
+                                                    <h1 class="hero">Эффективные решения<br />для вашего бизнеса</h1>
+                                                    <button class="btn" type="button">Заказать услуги</button>
+                                                </section>
+                                                <main>
+                                                    <section>
+                                                        <h2 hidden>Преимущество</h2>
+                                                        <ul class="middle">
+                                                            <li>
+                                                                <h3 class="link">Внимание к деталям</h3>
+                                                                <p class="description">Идейные соображения, а также<br />начало повседневной работы
+                                                                    по<br />формированию
+                                                                    позиции.</p>
+                                                            </li>
+                                                            <li>
+                                                                <h3 class="link">Пунктуальность</h3>
+                                                                <p class="description">Задача организации, в особенности<br />же рамки и место обучения
+                                                                    кадров<br />влечет за собой.
+                                                                </p>
+                                                            </li>
+                                                            <li>
+                                                                <h3 class="link">Планирование</h3>
+                                                                <p class="description">Равным образом консультация с<br />широким активом в
+                                                                    значительной<br />степени обуславливает.
+                                                                </p>
+                                                            </li>
+                                                            <li>
+                                                                <h3 class="link">Современные технологии</h3>
+                                                                <p class="description">Значимость этих проблем настолько<br />очевидна, что реализация
+                                                                    плановых<br />заданий.</p>
+                                                            </li>
+                                                        </ul>
+                                                    </section>
+                                                </main>
+                                                <section>
+                                                    <h2 class="work">Чем мы занимаемся</h2>
+                                                    <ul class="photos">
+                                                        <li><img src="./image/first.jpg" alt="work image" width="370" /></li>
+                                                        <li><img src="./image/second.jpg" alt="work image" width="370" /></li>
+                                                        <li><img src="./image/third.jpg" alt="work image" width="370" /></li>
+                                                    </ul>
+                                                </section>
+
+                                                <section>
+                                                    <h2 class="team">Наша команда</h2>
+                                                    <ul class="team-info">
+                                                        <li><img src="./image/two.jpg" alt="Игорь Демьяненко" width="270" />
+                                                            <h3 class="name">Игорь Демьяненко</h3>
+                                                            <p class="job" lang="en">Product Designer</p>
+                                                        </li>
+                                                        <li><img src="./image/three.jpg" alt="Ольга Репина" width="270" />
+                                                            <h3 class="name">Ольга Репина</h3>
+                                                            <p class="job" lang="en">Frontend Developer</p>
+                                                        </li>
+                                                        <li><img src="./image/four.jpg" alt="Николай Тарасов" width="270" />
+                                                            <h3 class="name"> Николай Тарасов</h3>
+                                                            <p class="job" lang="en">Marketing</p>
+                                                        </li>
+                                                        <li><img src="./image/five.jpg" alt="Михаил Ермаков" width="270" />
+                                                            <h3 class="name">Михаил Ермаков</h3>
+                                                            <p class="job" class lang="en">UI Designer</p>
+                                                        </li>
+                                                    </ul>
+
+                                                </section>
+                                                <footer>
+                                                    <address>
+                                                        <a class="logo1" href="">Web<span class="logo-mainer">Studio</span></a>
+                                                        <ul class="information">
+
+                                                            <li>
+                                                                <a class="reference " href="https://goo.gl/maps/5YBFwy5f6ECoQT7T9" target="_blank"
+                                                                    rel="noopener noreferrer">
+                                                                    г.Киев,пр-т Леси Украинки,26
+                                                                </a>
+
+                                                            </li>
+                                                            <li>
+                                                                <a class="phone-number" href="mailto:info@example.com">info@example.com</a>
+                                                            </li>
+                                                            <li>
+                                                                <a class="phone-number" href="tel:+380991111111">+380991111111</a>
+                                                            </li>
+                                                        </ul>
+                                                    </address>
+                                                </footer>
+                                            </body>
+
+                                        </html>
---
+++ prettier/prettier2.js
@@ -0,0 +1,129 @@
+< !DOCTYPE html >
+    <html lang="ru">
+
+        <head>
+            <link rel="stylesheet" href="./css/style.css" />
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
+                <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,700&display=swap" rel="stylesheet">
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+                        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;900&display=swap" rel="stylesheet">
+                            <link href="https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap" rel="stylesheet">
+                                <meta charset="UTF-8">
+                                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                            <title>Document</title>
+                                        </head>
+
+                                        <body>
+                                            <header>
+                                                <nav>
+                                                    <a class="logo" href="">Web<span class="logo-header">Studio</span>
+                                                    </a>
+
+                                                    <ul class="information">
+                                                        <li>
+                                                            <a class="linkers" href="./index.html">Студия</a>
+                                                        </li>
+                                                        <li>
+                                                            <a class="linkers" href="./portfolio.html">Портфолио</a>
+                                                        </li>
+                                                        <li>
+                                                            <a class="linkers" href="">Контакты</a>
+                                                        </li>
+                                                    </ul>
+                                                </nav>
+                                                <ul class="contacts-list">
+                                                    <li><a class="mail-link" href="mailto:info@devstudio.com">info@devstudio.com</a>
+                                                    </li>
+                                                    <li><a class="mail-link" href="tel:+380961111111">+380961111111</a>
+                                                    </li>
+                                                </ul>
+
+                                            </header>
+
+                                            <main>
+                                                <section>
+                                                    <h1 hidden>Портфолио</h1>
+                                                    <ul class="btn-list">
+                                                        <li>
+                                                            <button class="current" type="button">Все</button>
+                                                        </li>
+                                                        <li> <button class="current" type="button">Веб-сайты</button>
+                                                        </li>
+                                                        <li> <button class="current" type="button">Приложения</button>
+                                                        </li>
+                                                        <li> <button class="current" type="button">Дизайн</button>
+                                                        </li>
+                                                        <li> <button class="current" type="button">Маркетинг</button>
+                                                        </li>
+
+                                                    </ul>
+                                                    <ul class=" social-links">
+                                                        <li> <img src="./images/first.jpg" alt="Веб-сайт" width="370" />
+                                                            <h2 class="Goods">Технокряк</h2>
+                                                            <p class="description">Веб-сайт</p>
+                                                        </li>
+
+                                                        <li> <img src="./images/second.jpg" alt="Дизайн" width="370" />
+                                                            <h2 class="goods">Постер New Orlean vs Golden Star</h2>
+                                                            <p class="description">Дизайн</p>
+
+                                                        </li>
+                                                        <li> <img src="./images/third.jpg" alt="Приложение" width="370" />
+                                                            <h2 class="goods">Ресторан Seafood</h2>
+                                                            <p class="description">Приложение</p>
+                                                        </li>
+                                                        <li> <img src="./images/fourth.jpg" alt="Маркетинг" width="370" />
+                                                            <h2 class="goods">Проект Prime</h2>
+                                                            <p class="description">Маркетинг</p>
+                                                        </li>
+                                                        <li> <img src="./images/fivth.jpg" alt="Приложение" width="370" />
+                                                            <h2 class="goods">Проект Boxes</h2>
+                                                            <p class="description">Приложение</p>
+                                                        </li>
+                                                        <li> <img src="./images/sixth.jpg" alt="Веб-сайт" width="370" />
+                                                            <h2 class="goods"> Inspiration has no Borders</h2>
+                                                            <p class="description">Веб-сайт</p>
+                                                        </li>
+                                                        <li> <img src="./images/seven.jpg" alt="Дизайн" width="370" />
+                                                            <h2 class="goods">Издание Limited Edition</h2>
+                                                            <p class="description">Дизайн</p>
+                                                        </li>
+                                                        <li> <img src="./images/eight.jpg" alt="Маркетинг" width="370" />
+                                                            <h2 class="goods">Проект LAB</h2>
+                                                            <p class="description">Маркетинг</p>
+                                                        </li>
+                                                        <li> <img src="./images/nine.jpg" alt="Приложение" width="370" />
+                                                            <h2 class="goods">Growing Business</h2>
+                                                            <p class="description">Приложение</p>
+                                                        </li>
+                                                    </ul>
+                                                </section>
+                                            </main>
+
+                                            <footer>
+                                                <address>
+                                                    <a class="logo" href="">Web<span class="logo-footer">Studio</span>
+                                                    </a>
+                                                    <ul class="contacts2">
+
+                                                        <li>
+                                                            <a class="link1" href="https://goo.gl/maps/5YBFwy5f6ECoQT7T9" target="_blank"
+                                                                rel="noopener noreferrer">
+                                                                г.Киев,пр-т Леси Украинки,26
+                                                            </a>
+
+                                                        </li>
+                                                        <li>
+                                                            <a class="link2" href="mailto:info@example.com">info@example.com</a>
+                                                        </li>
+                                                        <li>
+                                                            <a class="link2" href="tel:+380991111111">+380991111111</a>
+                                                        </li>
+                                                    </ul>
+                                                </address>
+                                            </footer>
+
+                                        </body>
+
+                                    </html>
